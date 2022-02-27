38fd28f458e2ce7b77b28bb969fa566b095a2200
Mutti499 <75087023+Mutti499@users.noreply.github.com>
2022-02-27 23:55:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const hero = document.querySelector('.hero')
 const slider = document.querySelector('.slider')
 const logo = document.querySelector('#logo')
@@ -42,3 +43,25 @@ function myFunction(x) {
   
 var x = window.matchMedia("(max-width: 750px)")
 myFunction(x) // Call listener function at run time
+*/
+
+//Get the button:
+mybutton = document.querySelector(".myBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.classList.add("animate")
+    
+  } else {
+    mybutton.classList.remove("animate")
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
